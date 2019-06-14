735df0e6210e06ff97f332003ba3804d71e7ba56
limccn <limingcheng269@gmail.com>
2019-06-14 09:54:15 +0800

    create multi masked

--- utils/createMultiMasked.py
+++ utils/createMultiMasked.py
@@ -44,6 +44,10 @@ for pair in pair_filename:
     mask3=cv2.imread(mask3_path, cv2.IMREAD_COLOR)
     mask3_gray=cv2.cvtColor(mask3, cv2.COLOR_BGR2GRAY)
 
+    mask4_path=str(pair[0]).replace('test','mask-400000').replace('.tif','.pred.mask.tif')
+    mask4=cv2.imread(mask4_path, cv2.IMREAD_COLOR)
+    mask4_gray=cv2.cvtColor(mask4, cv2.COLOR_BGR2GRAY)
+
 
     # 3*3 GaussianBlur
     # gray = cv2.GaussianBlur(gray, (3, 3), 0)
@@ -56,7 +60,7 @@ for pair in pair_filename:
     #findContours()第一个参数是源图像、第二个参数是轮廓检索模式，第三个参数是轮廓逼近方法
     #输出是轮廓和层次结构，轮廓是图像中所有轮廓的python列表，每个单独的轮廓是对象边界点的(x,y)坐标的Numpy数组
     binary, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, contours, -1, (255, 0, 0), 2)
+    cv2.drawContours(image, contours, -1, (255, 255, 255), 2)
 
     # 3*3 GaussianBlur
     # gray = cv2.GaussianBlur(gray, (3, 3), 0)
@@ -83,7 +87,7 @@ for pair in pair_filename:
     #findContours()第一个参数是源图像、第二个参数是轮廓检索模式，第三个参数是轮廓逼近方法
     #输出是轮廓和层次结构，轮廓是图像中所有轮廓的python列表，每个单独的轮廓是对象边界点的(x,y)坐标的Numpy数组
     binary, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, contours, -1, (0, 255, 255), 1)
+    cv2.drawContours(image, contours, -1, (255, 0, 0), 1)
 
     # 3*3 GaussianBlur
     # gray = cv2.GaussianBlur(gray, (3, 3), 0)
@@ -97,6 +101,21 @@ for pair in pair_filename:
     #输出是轮廓和层次结构，轮廓是图像中所有轮廓的python列表，每个单独的轮廓是对象边界点的(x,y)坐标的Numpy数组
     binary, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(image, contours, -1, (0, 0, 255), 1)
+
+    
+    # 3*3 GaussianBlur
+    # gray = cv2.GaussianBlur(gray, (3, 3), 0)
+    # canny detect edge
+    mask4_gray = cv2.Canny(mask4_gray, 100, 300)
+
+    ret, thresh = cv2.threshold(mask4_gray, 127, 255, cv2.THRESH_BINARY)
+
+    # binary是最后返回的二值图像
+    #findContours()第一个参数是源图像、第二个参数是轮廓检索模式，第三个参数是轮廓逼近方法
+    #输出是轮廓和层次结构，轮廓是图像中所有轮廓的python列表，每个单独的轮廓是对象边界点的(x,y)坐标的Numpy数组
+    binary, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image, contours, -1, (255, 255, 0), 1)
+    
 
     #
     # cv2.imwrite('%s.1.png'%pair[1],binary,[int(cv2.IMWRITE_PNG_COMPRESSION),3])
